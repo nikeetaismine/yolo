043574297dbb26c44acd484cb30bca4eab9d56b3
nikeetaismine <denisenmwangi@gmail.com>
2025-04-19 01:54:10 +0300

    Changed mongo-url to the container instead of local

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb://yolo-ip-mongo:27017/yolomy';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
